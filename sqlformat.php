99a1f4e195e524d83ef1ca12ecc23987e72f90aa
Matthieu Moquet <matthieu@moquet.net>
2013-07-26 14:59:09 +0200

    Initial commit

---
+++ sqlformat.php
@@ -0,0 +1,51 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+$basename = $argv[0];
+
+$doc = <<<DOC
+Usage:
+  $basename [--no-format] [--no-highlight] [<sql>]
+  $basename [--no-comment] [--no-highlight] [<sql>]
+  $basename [-c|--compress] [<sql>]
+  $basename [-h | --help]
+
+Options:
+  --no-highlight  Disable highlight.
+  --no-format     Disable auto formatting.
+  --no-comment    Remove comment.
+  -c --compress   Compress SQL request.
+  -h --help       Display this help.
+DOC;
+
+$handler = new Docopt\Handler();
+$args = $handler->handle($doc);
+
+// Get SQL request for arguments or from stdin
+$sql = $args['<sql>'] ?: stream_get_contents(fopen("php://stdin", "r"));
+
+// Compress
+if ($args['--compress']) {
+    echo SqlFormatter::compress($sql);
+    return;
+}
+
+// No formatting
+if ($args['--no-format']) {
+    echo $args['--no-highlight'] ? $sql : SqlFormatter::highlight($sql);
+    return;
+}
+
+// No comment (implies format is enabled)
+if ($args['--no-comment']) {
+    $sql = SqlFormatter::removeComments($sql);
+}
+
+echo SqlFormatter::format($sql, !$args['--no-highlight']);
