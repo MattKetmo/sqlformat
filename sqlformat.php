42eb60183eba0a2a8321ad505ee649e8410d91d1
Matthieu Moquet <matthieu@moquet.net>
2015-02-14 17:31:33 +0100

    Add version arg

--- sqlformat.php
+++ sqlformat.php
@@ -3,7 +3,8 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-$basename = $argv[0];
+$version = "@package_version@";
+$basename = basename($argv[0]);
 
 $doc = <<<DOC
 Usage:
@@ -11,6 +12,7 @@ Usage:
   $basename [--no-comment] [--no-highlight] [<sql>]
   $basename [-c|--compress] [<sql>]
   $basename [-h | --help]
+  $basename [--version]
 
 Options:
   --no-highlight  Disable highlight.
@@ -18,10 +20,17 @@ Options:
   --no-comment    Remove comment.
   -c --compress   Compress SQL request.
   -h --help       Display this help.
+  -V --version    Show the version
 DOC;
 
 $handler = new Docopt\Handler();
 $args = $handler->handle($doc);
+
+// Show version
+if ($args['--version']) {
+  echo "$basename $version\n";
+  return;
+}
 
 // Get SQL request for arguments or from stdin
 $sql = $args['<sql>'] ?: stream_get_contents(fopen("php://stdin", "r"));
